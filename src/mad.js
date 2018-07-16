09b5b672b5d95b0b5cad1c6b5ac747a5ecb8f477
atlassubbed <atlassubbed@gmail.com>
2018-07-15 23:31:30 -0400

    implements mad and tests

--- src/mad.js
+++ src/mad.js
@@ -1,1 +1,11 @@
-console.log("Hello from atlas-mad!")
+const median = require("atlas-median");
+
+module.exports = (arr, isSorted) => {
+  let n = arr.length;
+  // short circuit all edge cases
+  if (!n) return;
+  if (n === 1) return 0;
+  const m = median(arr, isSorted), devs = [];
+  while(n--) devs[n] = Math.abs(m-arr[n]);
+  return median(devs)
+}
--- test/mad.test.js
+++ test/mad.test.js
@@ -1,23 +1,18 @@
 const { describe, it } = require("mocha")
 const { expect } = require("chai")
+const mad = require("../src/mad")
 
-describe.skip("mad", function(){
-  it("should return the mad of an odd-lengthed array", function(){
-    
-  })
-  it("should return the mad of an even-lengthed array", function(){
-
+describe("mad", function(){
+  it("should return the mad of an array of numbers", function(){
+    expect(mad([1,4,4,1,1])).to.equal(0)
   })
   it("should return undefined for a 0-array", function(){
-
+    expect(mad([])).to.be.undefined;
   })
   it("should return zero for a 1-array", function(){
-
-  })
-  it("should return the correct value for a 2-array", function(){
-
+    expect(mad([4])).to.equal(0);
   })
   it("should not sort the array if it is already sorted", function(){
-
+    expect(mad([1,4,4,1,1], true)).to.equal(3)
   })
 })
